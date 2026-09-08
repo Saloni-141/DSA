a7062949d706f16b568dca40d6131f0d78a1825d
Saloni Shaw <shawsaloni83@gmail.com>
2026-09-09 00:00:15 +0530

    Time: 7 ms (67.84%), Space: 43.8 MB (93.04%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+
+        for(int i=0; i<s1.length(); i++){
+            freq1[s1.charAt(i) - 'a']++;
+        }
+
+        int windsize = s1.length();
+
+        for(int i=0; i<s2.length(); i++){
+            freq2[s2.charAt(i) - 'a']++;
+            
+            if(i >= windsize){
+                freq2[s2.charAt(i - windsize) - 'a']--;
+            }
+
+            if(Arrays.equals(freq1,freq2)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
