80e9ba50c11dac82dc97d0a632a321add89ec280
Saloni Shaw <shawsaloni83@gmail.com>
2026-09-09 00:49:25 +0530

    Time: 6 ms (87.41%), Space: 43.9 MB (85.35%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         int[] freq1 = new int[26];
-       
+        int[] freq2 = new int[26];
 
         for(int i=0; i<s1.length(); i++){
             freq1[s1.charAt(i) - 'a']++;
@@ -10,13 +10,20 @@ class Solution {
         int windsize = s1.length();
 
         for(int i=0; i<s2.length(); i++){
-             int[] freq2 = new int[26];
-            int windidx = 0, idx = i;
+            // int[] freq2 = new int[26];
+            // int windidx = 0, idx = i;
 
-            while(windidx < windsize && idx < s2.length()){
-                freq2[s2.charAt(idx) - 'a']++;
-                windidx++;
-                idx++;
+            // while(windidx < windsize && idx < s2.length()){
+            //     freq2[s2.charAt(idx) - 'a']++;
+            //     windidx++;
+            //     idx++;
+            // }
+
+            freq2[s2.charAt(i) - 'a']++;
+
+            // Keep window size equal to s1.length()
+            if (i >= windsize) {
+                freq2[s2.charAt(i - windsize) - 'a']--;
             }
 
             if(Arrays.equals(freq1,freq2)){
